126c33396087d76a6db4f37a46e8cf9a56f55145
Kasitsyn <kasitsyn@gmail.com>
2021-11-26 13:16:43 +0700

    change some folders, create calc project

---
+++ LearnJS/learnJS.js
@@ -0,0 +1,69 @@
+//=============================== SetInterval, SetTimeOut
+
+const printNumbersSI = (from, to) => {
+    let count = from
+    let timerId = setInterval(() => {
+        if (count <= to) {
+            console.log(count)
+            count++
+        } else clearInterval(timerId)
+
+    }, 1000)
+}
+
+const printNumbersST = (from, to) => {
+    let count = from
+    let timerId = setTimeout(function tick() {
+        if (count <= to) {
+            console.log(count)
+            count++
+            timerId = setTimeout(tick, 1000)
+        } else clearTimeout(timerId)
+
+    }, 1000)
+}
+
+//=============================== Closure
+
+const sum = (a) => {
+    return function (b) {
+        return a + b
+    }
+}
+
+//-------------
+let arr = [1, 2, 3, 4, 5, 6, 7]
+
+const inBetween = (a, b) => {
+    return function (item) {
+        if (item >= a && item <= b) return true
+    }
+}
+
+const inArray = (array) => {
+    return function (item) {
+        return array.includes(item)
+    }
+}
+
+let res = arr.filter(inArray([1, 4, 22]))
+
+//----------
+
+let users = [
+    { name: "John", age: 20, surname: "Johnson" },
+    { name: "Pete", age: 18, surname: "Peterson" },
+    { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+
+const byField = (fieldName) => {
+    return function(a, b){
+        return a.fieldName > b.fieldName ? 1 : -1
+    }
+}
+
+users.sort(byField('surname'));
+console.log(users);
+
+
