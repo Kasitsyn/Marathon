4dd641f5bf30eb72056db1014b12a1c62d5820d3
Kasitsyn <kasitsyn@gmail.com>
2021-12-19 23:04:24 +0700

    added delete button in added locations

--- Weather_app/js/main.js
+++ Weather_app/js/main.js
@@ -1,37 +1,12 @@
 import { UI } from './view.js'
-
-UI.TABS_BTN.forEach((elem) => {
-    elem.addEventListener('click', (e) => {
-        UI.TABS_BTN.forEach((el) => {
-            el.classList.remove('tab__activated')
-        })
-        e.currentTarget.classList.add('tab__activated')
-    })
-})
-
-UI.TABS_BTN[0].click()
-
 const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather'
 const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f'
+const bookmarks = []
 
-UI.INPUT_CITY.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        getDataAndRenderNow(UI.INPUT_CITY.value)
-    }
-})
-
-UI.NOW_BOOKMARK.addEventListener('click', (e) => {
-    UI.LOCATIONS.insertAdjacentHTML('afterbegin', `<li><button class="locations__btn">${UI.NOW_CITY.textContent}</button></li>`);
-    UI.NOW_BOOKMARK.setAttribute('hidden', 'true')
-    UI.LOCATIONS_CITY = document.querySelector('.locations__btn')
-    UI.LOCATIONS_CITY.addEventListener('click', (e) => {
-        getDataAndRenderNow(e.currentTarget.textContent)
-        // console.log(e.currentTarget.textContent);
-    })
-    console.log(UI.LOCATIONS_CITY)
-
-})
-
+UI.TABS_BTN.forEach(makeCurrentTabActviated)
+UI.INPUT_CITY.addEventListener('keydown', (e) => e.key === 'Enter' ? getDataAndRenderNow(UI.INPUT_CITY.value) : '')
+UI.NOW_BOOKMARK.addEventListener('click', addToBookmarks)
+UI.TABS_BTN[0].click()
 
 function getDataAndRenderNow(city) {
     const cityName = city
@@ -45,11 +20,31 @@ function getDataAndRenderNow(city) {
             UI.NOW_TEMP.textContent = `${temp}°`
             UI.NOW_CITY.textContent = city
             UI.NOW_IMG.src = img
-            UI.NOW_BOOKMARK.removeAttribute('hidden')
-
+            if (!bookmarks.includes(city.toLowerCase())) UI.NOW_BOOKMARK.removeAttribute('hidden')
         })
         .catch(error => alert(error.message))
+        .finally(() => UI.INPUT_CITY.value = '')
 }
 
+function addToBookmarks(e) {
+    bookmarks.push(UI.NOW_CITY.textContent.toLowerCase())
+    UI.LOCATIONS.insertAdjacentHTML('afterbegin', `<li><button class="locations__btn">${UI.NOW_CITY.textContent}</button><button class="btn__close">X</button></li>`);
+    UI.NOW_BOOKMARK.setAttribute('hidden', 'true')
+    UI.LOCATIONS_CITY = document.querySelector('.locations__btn')
+    UI.LOCATIONS_CITY_DELETE = document.querySelector('.btn__close')
+    UI.LOCATIONS_CITY_DELETE.addEventListener('click', (e) => {
+        // bookmarks.find(e.currentTarget.textContent.toLowerCase())
+    })
+    UI.LOCATIONS_CITY.addEventListener('click', (e) => {
+        getDataAndRenderNow(e.currentTarget.textContent)
+    })
+}
 
-console.log(UI.LOCATIONS);
+function makeCurrentTabActviated(elem) {
+    elem.addEventListener('click', (e) => {
+        UI.TABS_BTN.forEach((el) => {
+            el.classList.remove('tab__activated')
+        })
+        e.currentTarget.classList.add('tab__activated')
+    })
+}
