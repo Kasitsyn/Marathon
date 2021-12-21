341416625b1bdaf921903e4d43a8df59767c78c4
Kasitsyn <kasitsyn@gmail.com>
2021-12-22 01:04:49 +0700

    working on localStorage

--- Weather_app/js/main.js
+++ Weather_app/js/main.js
@@ -1,38 +1,57 @@
 import { UI } from './view.js'
 const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather'
 const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f'
-const bookmarks = []
+const bookmarks = getBookmarks()
+
+const storage = {
+    img: '',
+    temperature: '',
+    city: ''
+}
 
 UI.TABS_BTN.forEach(makeCurrentTabActviated)
-UI.INPUT_CITY.addEventListener('keydown', (e) => e.key === 'Enter' ? getDataAndRenderNow(e) : '')
+UI.INPUT_CITY.addEventListener('keydown', (e) => e.key === 'Enter' ? renderNow(e) : '')
 UI.NOW_BOOKMARK.addEventListener('click', addToBookmarks)
 UI.TABS_BTN[0].click()
+renderBokkmarks()
 
-function getDataAndRenderNow(e) {
+function renderNow(e) {
+    getData(e).then((resp) => {
+        UI.NOW_TEMP.textContent = `${storage.temperature}°`
+        UI.NOW_CITY.textContent = storage.city
+        UI.NOW_IMG.src = storage.img
+
+        UI.INPUT_CITY.value = ''
+
+    })
+
+}
+
+function getData(e) {
     let cityName
     if (e.currentTarget.className === 'search__input') cityName = e.currentTarget.value
     else cityName = e.currentTarget.textContent
-
     const url = `${SERVER_URL}?q=${cityName}&appid=${API_KEY}`
-    fetch(url)
+    return fetch(url)
         .then(response => response.json())
         .then(data => {
-            const img = `http://openweathermap.org/img/wn/${data.weather.find(elem => elem.icon).icon}@2x.png`
-            const temp = Math.round(data.main.temp - 273.15)
-            const city = data.name
-            UI.NOW_TEMP.textContent = `${temp}°`
-            UI.NOW_CITY.textContent = city
-            UI.NOW_IMG.src = img
-            if (!bookmarks.includes(city.toLowerCase())) UI.NOW_BOOKMARK.removeAttribute('hidden')
+            storage.img = `http://openweathermap.org/img/wn/${data.weather.find(elem => elem.icon).icon}@2x.png`
+            storage.temperature = Math.round(data.main.temp - 273.15)
+            storage.city = data.name
+            // localStorage.getDataStorage = JSON.stringify(storage)
+
         })
         .catch(error => alert(error.message))
-        .finally(() => UI.INPUT_CITY.value = '')
+
 }
 
 function addToBookmarks(e) {
+
     bookmarks.push(UI.NOW_CITY.textContent.toLowerCase())
     UI.NOW_BOOKMARK.setAttribute('hidden', 'true')
-    localStorage.setItem('bookmarks', bookmarks)
+    localStorage.bookmarks = JSON.stringify(bookmarks)
+
+
     renderBokkmarks()
 }
 
@@ -60,8 +79,9 @@ function renderBokkmarks() {
         UI.LOCATIONS_CITY = document.querySelector('.locations__btn')
         UI.LOCATIONS_CITY_DELETE = document.querySelector('.btn__close')
         UI.LOCATIONS_CITY_DELETE.addEventListener('click', deleteBookmark)
-        UI.LOCATIONS_CITY.addEventListener('click', getDataAndRenderNow)
+        UI.LOCATIONS_CITY.addEventListener('click', renderNow)
     })
+    if (!bookmarks.includes(storage.city.toLowerCase())) UI.NOW_BOOKMARK.removeAttribute('hidden')
 
 }
 
@@ -69,4 +89,11 @@ function renderBokkmarks() {
 // const favoriteCities = storage.getFavoriteCities();
 // const currentCity = storage.getCurrentCity();
 
-// console.log(localStorage.getItem('bookmarks'))
+function saveBookmarks() {
+    localStorage.bookmarks = JSON.stringify(bookmarks)
+}
+
+function getBookmarks() {
+    if (localStorage.bookmarks) return JSON.parse(localStorage.bookmarks)
+    else return []
+}
