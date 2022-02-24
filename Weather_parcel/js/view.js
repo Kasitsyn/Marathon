5d09cbf99a438c2d01568a8aefa85a29eb20f78f
kasitsyn <kasitsyn@gmail.com>
2022-02-25 00:28:59 +0700

    weather_parcer installed npm, parcel, date-fn, gitignor

---
+++ Weather_parcel/js/view.js
@@ -0,0 +1,12 @@
+export const UI = {
+    TABS_BTN: document.querySelectorAll('.tabs__item'),
+    TABS_CONTENT: document.querySelectorAll('.tabs__block'),
+    INPUT_CITY: document.querySelector('.search > input'),
+    NOW_TEMP: document.querySelector('.temp'),
+    NOW_CITY: document.querySelector('.bottom > span'),
+    NOW_IMG: document.querySelector('.now > img'),
+    NOW_BOOKMARK: document.querySelector('.bookmark'),
+    LOCATIONS: document.querySelector('.locations__added > ul'),
+    DETAILS: document.querySelector('#tab_02'),
+    FORECAST: document.querySelector('#tab_03')
+}
