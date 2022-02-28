1b97518f55b80ea49126cc45ef312ff3871ca4a9
kasitsyn <kasitsyn@gmail.com>
2022-02-28 18:14:24 +0700

    layout first page

---
+++ Weather_parcel/js/storage.js
@@ -0,0 +1,21 @@
+class Storage {
+    constructor(key, defaultValue, storageType) {
+        this.key = key
+        this.defaultValue
+        this.storageType
+    }
+    get() {
+
+    }
+    set() {
+
+    }
+    clear() {
+
+    }
+    isEmpty() {
+
+    }
+}
+
+const bookmarks = new Storage([rome, paris], defaultValue, storageType)
